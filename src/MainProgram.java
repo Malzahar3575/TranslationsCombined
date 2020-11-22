d9165a2c629b35dcdb9417985f03540124aac174
Malzahar3575 <45061833+Malzahar3575@users.noreply.github.com>
2020-11-22 23:28:41 +0900

    test

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -29,11 +29,11 @@ public class MainProgram {
 		
 //		System.out.println(result.getResultText());
 		
-		String url = "https://www.google.com/search?q=jeb+bush+barack+obama";
+		String url = "https://www.google.com/search?q=cats";
 		try {
 		Document document = Jsoup //
 		                   .connect(url) //
-		                   .userAgent("Mozilla/5.0 (Windows; U; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)") //
+		                   .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36") //
 		                   .get();
 		
 		Element divResultStats = document.select("div#result-stats").first();
